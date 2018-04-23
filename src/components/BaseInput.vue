d22bd9af0a78ad91103c27e765caf48f9988704d
Drew Wilson <drew@geedew.com>
2018-04-23 08:24:57 -0500

    Adding a base input that I can build from

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,41 @@
+<script>
+let uid = 0;
+export default {
+  name: "BaseInput",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+      validator: val => ["text", "email"].includes(val)
+    },
+    placeholder: {
+      type: String,
+      default() {
+        return "";
+      }
+    },
+    label: {
+      type: String,
+      validation(label) {
+        return label.length > 1;
+      }
+    },
+    data() {
+      return {
+        // A simple unique id for instance
+        id: `text-${Date.now()}${uid++}`
+      };
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <label v-if="label" :for="id">{{label}}</label>
+    <input
+        :id="id"
+        :placeholder="placeholder"
+        v-bind="$attrs"> 
+  </div>
+</template>
