7f86848d4b7c4889e20fc82e4d48174389fb3aef
Drew Wilson <drew@geedew.com>
2018-04-23 08:17:15 -0500

    Getting a basic form into the page

---
+++ src/views/Email.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form id="email" name="email" action="">
+            <datalist id="emails">
+            <option value="drew@geedew.com">Drew Wilson</option>
+            </datalist>
+            <fieldset>
+            <legend>Correspondence</legend>
+            <label for="email+to">To</label>
+            <input
+                type="email"
+                id="email+to"
+                name="email+to"
+                list="emails"
+                autocomplete="email"
+                placeholder="Enter a name to search"
+                required
+                autofocus>
+            <br>
+            <label for="email+cc">CC</label>
+            <input
+                type="email"
+                list="emails"
+                id="email+cc"
+                name="email+cc"
+                autocomplete="email"
+                placeholder="Enter more names to search"
+                multiple>
+            </fieldset>
+            <fieldset>
+            <legend>Communication</legend>
+            <label for="email+subject">Subject</label>
+            <input id="email+subject" name="email+subject">
+            <br>
+            <label for="email+body">Body</label>
+            <textarea id="email+body" name="email+body"></textarea>
+            </fieldset>
+
+            <input type="reset" value="reset">
+            <button type="submit">Send some mail!</button>
+        </form>
+    </div>
+</template>
